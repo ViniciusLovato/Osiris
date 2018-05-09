87828dfb90d07b57b0825b7f3a38a118a3bffbc9
Matheus Compri <matheus.compri@gmail.com>
2018-05-09 00:32:17 -0300

    #15 Improving ticket list UI

--- client/src/ticketsOverview/listItem/ListItem.js
+++ client/src/ticketsOverview/listItem/ListItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './ListItem.css';
 
 const propTypes = {
   subject: PropTypes.string.isRequired,
@@ -8,30 +9,71 @@ const propTypes = {
   priority: PropTypes.string.isRequired
 }
 
+const Avatar = ({name, pic}) => {
+  return(
+    <div class="user-avatar">
+      <div class="pic"></div>
+      <span class="name">{name}</span>
+    </div>
+  );
+};
+
+const TicketBasicInfo = ({subject, description}) => {
+  return(
+    <div class="ticket-info">
+      <div class="header">
+        <span class="subject">{subject}</span><span class="timestamp">Wednesday, May 28 at 2 p.m.</span>
+      </div>
+      <div>
+        <span class="description">{description || `no description `}</span>
+      </div>
+    </div>
+  );
+};
+
+const TicketStatus = ({status}) => {
+  return(
+    <div class="ticket-status">
+      <div class="status"></div>
+      <span class="description">{status}</span>
+    </div>
+  );
+};
+
+const TicketProject = ({name, tags}) => {
+  return(
+    <div class="project-info">
+      <header class="project">{name}</header>
+      <Tags />
+    </div>
+  );
+};
+
+const Tags = ({name}) => {
+  return(
+    <div class="tag-info">
+      <span class="tag is-danger">Bug</span>
+      <span class="tag is-info">Feature</span>
+    </div>
+  );
+};
+
 class ListItem extends Component {
   render() {
+    const userNames = ['John', 'Dave', 'Frank', 'Edgar'];
+    const projectNames = ['Sigma', 'Zeta', 'Epsilon'];
+    const status = ['New', 'Open', 'Pending', 'On-hold', 'Solved', 'Closed'];
+    
     return (
       <li>
-        <div class="card">
-
+        <div className={'card ' + (this.props.priority)}>
           <div class="card-content">
-            <div class="media">
-              {/* <div class="media-left">
-                <figure class="image is-48x48">
-                  <img src={this.props.avatar} alt="Placeholder"/>
-                </figure>
-              </div> */}
-              <div class="media-content">
-                <p class="title is-4">{this.props.subject}</p>
-                <p class="subtitle is-6">{this.props.priority}</p>
-              </div>
-            </div>
-
-            <div class="content">
-              {this.props.description ||  `no description `}
-              <br/>
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+            <Avatar name={userNames[Math.floor(Math.random() * userNames.length)]}/>
+            <TicketBasicInfo subject={this.props.subject}
+              description={this.props.description}
+              timestap={this.props.timestamp} />
+            <TicketStatus status={status[Math.floor(Math.random() * status.length)]}/>
+            <TicketProject name={projectNames[Math.floor(Math.random() * projectNames.length)]}/>
           </div>
         </div>
       </li>
