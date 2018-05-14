fd59967c02c88317e47334a71cc8b3406b6cbee8
Vinicius Lovato <lovato.vinicius@gmail.com>
2018-05-13 23:33:33 -0300

    Added buttons set to control list. Fixed media queries

--- client/src/components/Ticket/HorizontalButtons/HorizontalButtons.js
+++ client/src/components/Ticket/HorizontalButtons/HorizontalButtons.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
-import Button from '../../UI/Elements/Button'
 import './HorizontalButtons.css';
 
+// TODO fix this
+// Crate smaller functional components for this 
 const HorizontalButtons = () => (
   <div className="horizontal-buttons-container">
-    <Button>New ticket</Button>
+    <div className="control">
+      <div className="select">
+        <select>
+          <option>Order by</option>
+          <option>Assignee</option>
+          <option>Date</option>
+          <option>Last Update</option>
+        </select>
+      </div>
+    </div>
+    <div className="control">
+      <div className="field has-addons">
+        <div className="control">
+          <input className="input" type="text" placeholder="Quick filter"/>
+        </div>
+        <div className="control">
+          <a className="button is-info">
+            Apply
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="control">
+      <a className="button is-primary">New Ticket</a>
+    </div>
   </div>
 );
+
+
 
 
 export default HorizontalButtons;
